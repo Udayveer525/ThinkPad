84c9693e815131b3e1bdfcf0c0bb1e5c19719a7d
Udayveer525 <152760915+Udayveer525@users.noreply.github.com>
2025-08-25 20:37:12 +0530

    Prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,18 +5,23 @@ import cors from "cors";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "../src/config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 // middleware
-app.use(cors(
-  {
-    origin: 'http://localhost:5173',
-  }
-))
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors(
+    {
+      origin: 'http://localhost:5173',
+    }
+  ))
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 // app.use((req, res, next) => {
@@ -26,11 +31,16 @@ app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+  });
+}
+
 connectDB().then(() => {
   app.listen(PORT, () => {
   console.log("Server is running on PORT: ", PORT);
   });
 })
-
-
-//mongodb+srv://uvsu525:yrPkVc7Qgv0pYihv@cluster0.aya9vuw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
